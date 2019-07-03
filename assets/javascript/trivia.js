14575c1c78c6f143eeb70d65ffef99a24a43c8b2
pyronasaur <pyronnicholas@gmail.com>
2019-07-03 12:39:42 -0400

    Game logic working with correct answer counts and screen updates

--- assets/javascript/trivia.js
+++ assets/javascript/trivia.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     var clockRunning = false;
     var time = 31; //31 so user sees 30 to start and gets the full time
     var intervalId;
+
+    var correctCount = 0;
+
     window.onload = start;
 
     function start() {
@@ -14,13 +17,50 @@ $(document).ready(function(){
 
     function count() {
         time--;
-        console.log(time);
+        //console.log(time);
         if (time <= 0) {
             clearInterval(intervalId);
             clockRunning = false;
+            countAnswers;
+            endGame(correctCount);
         }
 
-        // DONE: Use the variable we just created to show the converted time in the "display" div.
         $("#timer").text(time);
+    }
+
+    $("#submit").on("click", function(){
+        console.log("button was clicked");
+
+        countAnswers();
+        endGame(correctCount);
+
+    })
+
+    function countAnswers(){
+        var q = $("input:checked");
+        console.log(q);
+
+        for(let item of q) {
+            console.log(item);
+            console.log(item.checked);
+            if(item.value === "arthur" ||
+            item.value === "grail" ||
+            item.value === "blue" ||
+            item.value === "which") {
+                correctCount++;
+            }
         }
+        console.log("correctCount: " + correctCount);
+    }
+
+    function endGame(score) {
+        
+        var endDiv = $("<div>");
+        $("#game-holster").empty();
+        $(endDiv).addClass("game-item");       
+
+        $("#game-holster").append(endDiv);
+        $(endDiv).html("<p>You got " + correctCount + " correct!</p>");
+        console.log(endDiv.text);
+    }
 });
